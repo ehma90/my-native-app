f1228536b7ef3f2b159da322946aa15e4c4be082
Emmanuel Essien <ehmaessien@gmail.com>
2023-07-16 13:08:00 +0100

    Update todoItem

--- Desktop/react-native/mynativeproject/components/todosItem.js
+++ Desktop/react-native/mynativeproject/components/todosItem.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function TodosItem({item, pressHandler}){
-
-    return (
-        <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
-        </TouchableOpacity>
-    )
+export default function TodosItem({ item, pressHandler }) {
+  return (
+    <TouchableOpacity onPress={() => pressHandler(item.key)}>
+      <View style={styles.item}>
+        <MaterialIcons  name="delete" size={18} color="#333" />
+        <Text>{item.text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    item: {
-        padding: 16,
-        marginTop: 16,
-        borderColor: '#6c757d',
-        borderWidth: 1,
-        borderStyle: 'dashed',
-        borderRadius: 10,
-    }
-})
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: "#6c757d",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 10,
+    flexDirection: 'row',
+    gap: 8,
+
+  },
+});
